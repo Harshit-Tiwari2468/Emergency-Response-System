c7f73d8b388583ae693398ce83aaaf2fd826a8c8
Harshit-Tiwari2468 <168590428+Harshit-Tiwari2468@users.noreply.github.com>
2025-07-27 22:11:46 +0000

    Learn datatypes

---
+++ JavaScript/lec-02.js
@@ -0,0 +1,27 @@
+"use strict" // uses all the JS code as newer version
+
+// alert ( 3 + 3 ) shows error, because we are not using browser
+
+// https://tc39.es/ecma262/#sec-intro , visit this url to read documentation of JavaScript.
+// Standars of javascript was developed by ECMAScript
+
+// data_types :-
+
+// 1. number 
+    let num = 123456789;
+// 2. string => ""
+    let str = "Harshit";
+// 3. boolean => true/false
+    let bool = true;
+    let b = false;
+// 4. null => standalone value
+    let n = null;
+// 5. undefined
+    let u = undefined;
+// 6. symbol => unique
+// 7. object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+console.table([num,str,bool,b,n,u]);
