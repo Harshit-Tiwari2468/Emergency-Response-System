fee9acc18efeae37b75156570657d6e1d95a355f
Harshit-Tiwari2468 <168590428+Harshit-Tiwari2468@users.noreply.github.com>
2025-07-27 21:43:18 +0000

    Learnings

--- JavaScript/lec-01.js
+++ JavaScript/lec-01.js
@@ -3,5 +3,17 @@ var user_name = "Harshit"
 let user_Surname = "Tiwari"
 user_email = "ht.harshit1627@gmail.com"
 
-console.log(user_id);
-console.table([user_id,user_name,user_Surname,user_email]);
+let emergency_type;
+let emergency_location;
+
+// accountId = 2; not allowed
+
+// prefer not to use var because of issue in block scope and functional scope
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(user_id,user_name,user_Surname,user_email);
+console.table([user_id,user_name,user_Surname,user_email,emergency_type,emergency_location]);
